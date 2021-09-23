9fc3caa720e5b622409e4ca442cff2e00bdddc18
Christian <freitaschristian1204@gmail.com>
2021-09-23 11:19:39 -0300

    ok

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://devscholl-heroku.herokuapp.com/'
+    baseURL: 'https://devscholl-heroku.herokuapp.com'
 })
 
 export default class Api {
